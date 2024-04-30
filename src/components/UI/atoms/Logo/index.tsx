090f1a906b36163beecafcb481731f01aa982cf8
adrianbgo <adrianbgo@hotmail.com>
2024-04-30 08:56:25 -0400

    chore: added logo component structure

---
+++ src/components/UI/atoms/Logo/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from "./index.module.css";
+
+const Logo = () => {
+  return (
+    <div data-testid="logo" className={styles.logoContainer}>
+      <p>Logo</p>
+    </div>
+  );
+};
+
+export default Logo;
