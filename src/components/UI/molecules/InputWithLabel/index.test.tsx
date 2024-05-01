d19021fa393dfba36fd908db9a60b5bcc5e2af67
adrianbgo <adrianbgo@hotmail.com>
2024-05-01 15:47:38 -0400

    feat: added InputWithLabel and validation

---
+++ src/components/UI/molecules/InputWithLabel/index.test.tsx
@@ -0,0 +1,124 @@
+import "@testing-library/jest-dom";
+import { fireEvent, render, screen } from "@testing-library/react";
+import InputWithLabel from ".";
+import { FormProvider, useForm } from "react-hook-form";
+import { PropsWithChildren } from "react";
+import userEvent from "@testing-library/user-event";
+
+const FormWrapper: React.FC<PropsWithChildren> = ({ children }) => {
+  const methods = useForm({
+    mode: "onChange",
+  });
+
+  const handleSubmit = methods.handleSubmit((data) => console.log(data));
+
+  return (
+    <FormProvider {...methods}>
+      <form noValidate autoComplete="off" onSubmit={(e) => e.preventDefault()}>
+        {children}
+        <button onClick={handleSubmit}>Submit</button>
+      </form>
+    </FormProvider>
+  );
+};
+
+const setup = (jsx: React.JSX.Element) => {
+  return {
+    user: userEvent.setup(),
+    ...render(jsx),
+  };
+};
+
+describe("InputWithLabel", () => {
+  it("renders with default input", () => {
+    setup(
+      <FormWrapper>
+        <InputWithLabel
+          id="test-input"
+          label="Test Label"
+          validation={{}}
+          value="testVal"
+        />
+      </FormWrapper>
+    );
+
+    const input = screen.getByTestId("input");
+    const label = screen.getByTestId("label");
+
+    expect(input).toBeInTheDocument();
+    expect(input).toHaveValue("testVal");
+    expect(label).toBeInTheDocument();
+    expect(label).toHaveTextContent("Test Label");
+  });
+
+  it("renders with long input", () => {
+    setup(
+      <FormWrapper>
+        <InputWithLabel
+          id="test-input"
+          label="Test Label"
+          validation={{}}
+          value="testVal"
+          inputType="long"
+        />
+      </FormWrapper>
+    );
+
+    const input = screen.getByTestId("input-long");
+
+    expect(input).toBeInTheDocument();
+    expect(input).toHaveRole("textbox");
+  });
+
+  it("renders short validation error", async () => {
+    const { user } = setup(
+      <FormWrapper>
+        <InputWithLabel
+          id="test-input"
+          label="Test Label"
+          validation={{
+            required: {
+              value: true,
+              message: "required",
+            },
+          }}
+        />
+      </FormWrapper>
+    );
+
+    const submit = screen.getByRole("button");
+
+    await user.click(submit);
+
+    const error = screen.getByTestId("input-error");
+    expect(error).toBeInTheDocument();
+    expect(error).toHaveTextContent("required");
+  });
+
+  it("renders long validation error", async () => {
+    const { user } = setup(
+      <FormWrapper>
+        <InputWithLabel
+          id="test-input"
+          label="Test Label"
+          validation={{
+            required: {
+              value: true,
+              message: "required",
+            },
+          }}
+          value=""
+          inputType="long"
+        />
+      </FormWrapper>
+    );
+
+    const submit = screen.getByRole("button");
+
+    await user.click(submit);
+
+    const error = screen.getByTestId("input-error");
+    expect(error).toBeInTheDocument();
+    expect(error).toHaveTextContent("required");
+  });
+});
---
+++ src/components/UI/molecules/InputWithLabel/index.tsx
@@ -0,0 +1,112 @@
+import {
+  findInputError,
+  isFormInvalid,
+} from "@/utils/formValidation/formValidation";
+import { AnimatePresence, motion } from "framer-motion";
+import React, { ChangeEvent, FC } from "react";
+import { FieldValues, RegisterOptions, useFormContext } from "react-hook-form";
+import { MdError } from "react-icons/md";
+import styles from "./index.module.css";
+
+interface IInputWithLabel {
+  label: string;
+  id: string;
+  value?: string;
+  validation?: RegisterOptions<FieldValues, string>;
+  inputType?: "text" | "long" | "email" | "url";
+  placeholder?: string;
+}
+
+const InputWithLabel: FC<IInputWithLabel> = ({
+  label,
+  id,
+  value,
+  validation,
+  inputType = "text",
+  placeholder,
+}) => {
+  const {
+    register,
+    formState: { errors },
+  } = useFormContext();
+
+  const inputError = findInputError(errors, label);
+  const isInvalid = isFormInvalid(inputError);
+
+  return inputType === "long" ? (
+    <div className={styles.container}>
+      <div className={styles.labelContainer}>
+        <label data-testid="label" htmlFor={id} className={styles.label}>
+          {label}
+        </label>
+        <AnimatePresence mode="wait" initial={false}>
+          {isInvalid && (
+            <InputError
+              message={inputError.error!.message!}
+              key={inputError.error!.message!}
+            />
+          )}
+        </AnimatePresence>
+      </div>
+      <textarea
+        placeholder={placeholder || ""}
+        data-testid="input-long"
+        id={id}
+        value={value}
+        {...register(label, validation)}
+        className={styles.inputLong}
+      />
+    </div>
+  ) : (
+    <div className={styles.container}>
+      <div className={styles.labelContainer}>
+        <label data-testid="label" htmlFor={id} className={styles.label}>
+          {label}
+        </label>
+        <AnimatePresence mode="wait" initial={false}>
+          {isInvalid && (
+            <InputError
+              message={inputError.error!.message!}
+              key={inputError.error!.message!}
+            />
+          )}
+        </AnimatePresence>
+      </div>
+      <input
+        type={inputType}
+        placeholder={placeholder || ""}
+        data-testid="input"
+        id={id}
+        value={value}
+        className={styles.input}
+        {...register(label, validation)}
+      />
+    </div>
+  );
+};
+
+interface IInputError {
+  message: string;
+}
+
+const InputError: FC<IInputError> = ({ message }) => {
+  return (
+    <motion.p
+      {...framer_error}
+      className={styles.inputError}
+      data-testid="input-error"
+    >
+      <MdError />
+      {message}
+    </motion.p>
+  );
+};
+
+const framer_error = {
+  initial: { opacity: 0, y: 10 },
+  animate: { opacity: 1, y: 0 },
+  exit: { opacity: 0, y: 10 },
+  transition: { duration: 0.2 },
+};
+
+export default InputWithLabel;
