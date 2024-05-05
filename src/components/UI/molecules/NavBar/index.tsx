a8e3334199af1d7f80abf17950d89506909df75c
adrianbgo <adrianbgo@hotmail.com>
2024-05-05 10:32:05 -0400

    style: added responsiveness to NavBar component

--- src/components/UI/molecules/NavBar/index.tsx
+++ src/components/UI/molecules/NavBar/index.tsx
@@ -1,14 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./index.module.css";
 import NavLink from "../../atoms/NavLink";
 import Logo from "../../atoms/Logo";
+import { FaBars } from "react-icons/fa6";
 const NavBar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
     <nav data-testid="nav-bar" className={styles.navBar}>
       <div>
         <Logo />
       </div>
-      <div className={styles.links}>
+      <a
+        href="javascript:void(0)"
+        className={styles.icon}
+        onClick={() => setIsOpen(!isOpen)}
+      >
+        <FaBars />
+      </a>
+      <div className={`${styles.links} ${isOpen && styles.linksShown}`}>
         <NavLink href="/">Home</NavLink>
         <NavLink href="/adventures">Adventure Writing</NavLink>
         <NavLink href="/blog">Blog</NavLink>
